05dd8f00454a7046fe7bb669a39b2ada98966314
Firebyte Studio <firebyte_studio@outlook.com>
2025-12-23 15:48:39 -0300

    Update neural_voice.h

--- engine/core/voice/neural_voice.h
+++ engine/core/voice/neural_voice.h
@@ -1,19 +1,20 @@
+#pragma once
+#include "voice_base.h"
 #include "../neural/neural_onnx.h"
 
 namespace auraloid {
 
-class NeuralVoice : public IVoice {
+class NeuralVoice : public VoiceBase {
 public:
-    NeuralVoice();
+    bool loadModel(const void* data, size_t size);
 
-    bool loadModel(const std::string& path);
-
-    AudioBuffer<float> render(
-        const RenderRequest& request
+    AudioBuffer<float> synthesize(
+        const SeqTrack& track,
+        int sampleRate
     ) override;
 
 private:
-    NeuralONNX m_onnx;
+    NeuralONNX m_runtime;
 };
 
 }
